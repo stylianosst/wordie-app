7c6696f28faf5a1299492eb60fcaf6b42084b085
Stylianos Plus <stylianosplus@gmail.com>
2025-02-17 01:11:47 +0200

    ff

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
-export default function (api) {
+// export default function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: []
+//   };
+// };
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: []
   };
 };
